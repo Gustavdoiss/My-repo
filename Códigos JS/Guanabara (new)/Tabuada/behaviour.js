a1389d0a5caad7102c513666dada4952da081e41
Gustavdoiss <gustavo.eduacc@gmail.com>
2026-04-18 01:04:21 -0300

    Tabuada 02

--- Códigos JS/Guanabara (new)/Tabuada/behaviour.js
+++ Códigos JS/Guanabara (new)/Tabuada/behaviour.js
@@ -1,17 +1,20 @@
-let num, button, cont = 1, res = [];
-num = document.getElementById('tabNum');
+let input, button, cont = 1, res, itens = [];
+input = document.getElementById('tabNum');
 button = document.getElementById('doTable');
-res = document.querySelectorAll('li');
+res = document.querySelector('ul')
 
 button.addEventListener('click', function() {
-    num = Number(num.value);
-    if (num == null) {
+    res.innerHTML = '';
+    let num = Number(input.value);
+    cont = 1;
+    if (num === null || Number.isNaN(num) || num === 0) {
         window.alert(`Por favor, digite um valor válido!!`);
     }else {
-        for (let i = 0; i < res.length; i++) {
-        res[i].innerHTML = `${num} x ${cont} = ${cont*num}`;
-        cont++;
+        for (let i = 0; i < 30; i++) {
+            itens[i] = document.createElement('li');
+            itens[i].innerHTML = `${num} x ${cont} = ${cont*num}`;
+            res.appendChild(itens[i]);
+            cont++;
         }
     }
-
 })
