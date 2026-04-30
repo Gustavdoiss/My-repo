b8ca121677298bf0b52a136891dccb3f4fe0540d
Gustavo de Sena <gustavo.eduacc@gmail.com>
2026-04-30 10:53:41 -0300

    BEE 2670

--- Códigos C/BEE_2670.c
+++ Códigos C/BEE_2670.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
- 
+
 int main() {
 
-    
+    int a, b , c, w, x, y, z;
+    scanf("%d\n%d\n%d", &a, &b, &c);
+    x = (b * 2 + c * 4);
+    y = (a + c) * 2;
+    z = (b * 2 + a * 4);
+    if (x <= y && x <= z) printf("%d\n", x);
+    else if (y <= x && y <= z) printf("%d\n", y);
+    else if (z <= x && z <= y) printf("%d\n", z);
+
+
     return 0;
 }
